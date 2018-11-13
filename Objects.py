d62a561e8f902389063f247800173b165ec84db0
Unknown <zckd78@gmail.com>
2018-11-13 12:05:44 -0800

    Basic Game Engine + Player
    Created another file to serve as the GameEngine, which holds the Grid, which itself holds all the objects. Created some test scenarios in the Main function to highlight functions coded.

--- Objects.py
+++ Objects.py
@@ -5,6 +5,27 @@ import Utils
 # Create new Classes here
 # ================================
 
+class EnumCadence:
+    SNEAK = 0
+    WALK = 1
+    RUN = 2
+    STR_SNEAK = "SNEAK"
+    STR_WALK = "WALK"
+    STR_RUN = "RUN"
+
+
+class EnumDirection:
+    NORTH = 0
+    EAST = 1
+    SOUTH = 2
+    WEST = 3
+    STR_NORTH = "NORTH"
+    STR_EAST = "EAST"
+    STR_SOUTH = "SOUTH"
+    STR_WEST = "WEST"
+
+
+
 # Tile Class is the basic floor tile.
 class Tile:
     def __init__(self, x, y):
@@ -14,6 +35,7 @@ class Tile:
         # Chances are separated by [SNEAK, WALK, RUN]
         self.TransitionChance = [1.0, 1.0, 1.0]
 
+    # When you use str() on a Tile object, this function is called
     def __str__(self):
         x, y = self.GetCoords()
         return self.Name + " (" + str(x) + "," + str(y) + ")"
@@ -23,7 +45,6 @@ class Tile:
 
     def GetTile(self):
         return "[ ]"
-
 
 # Wall to block player movement
 class Wall(Tile):
@@ -58,96 +79,57 @@ class TrapDoor(Tile):
     def GetTile(self):
         return "[/]"
 
-class GameGrid:
-    def __init__(self):
-        # Size of the grid
-        self.Size = 10
 
-        # Total number of blocks
-        self.Dimensions = self.Size * self.Size
+class Player:
+    def __init__(self, X, Y):
+        self.posX = X
+        self.posY = Y
+        self.Tired = False
+        self.Exhausted = False
+        self.ChanceToExhaust = 0.05
+        self.ExhaustedTurns = 0
+        self.Cadence = EnumCadence.WALK
+        self.DebugMode = True
 
-        # Arrays of Tiles, Walls, and Traps
-        self.Tiles = []
-        self.Walls = []
-        self.TrapDarts = []
-        self.TrapDoors = []
+    def isTired(self):
+        return self.IsTired
 
-    def RandomizeLevel(self):
-        # Handle Columns
-        for y in range(0, self.Size, 1):
-            # Handle Rows
-            for x in range(0, self.Size, 1):
-                # print(str(x) + "," + str(y))
-                # Test for what tile we should create at what position
+    def isExhausted(self):
+        return self.Exhausted
 
-                # 20% chance of Trap Door
-                doorChance = Utils.TryChance(0.20)
-                if doorChance:
-                    self.TrapDoors.append(TrapDoor(x, y))
-                    continue
+    def GetCoords(self):
+        return self.posX, self.posY
 
-                # 25% of Darts
-                dartChance = Utils.TryChance(0.35)
-                if dartChance:
-                    self.TrapDarts.append(TrapDarts(x, y))
-                    continue
+    def GetTile(self):
+        return "[P]"
 
-                # 30% chance of a wall
-                wallChance = Utils.TryChance(0.7)
-                if wallChance:
-                    self.Walls.append(Wall(x, y))
-                    continue
+    def WalkOrSneak(self):
+        # Remove an exhausted turn
+        if self.ExhaustedTurns > 0:
+            self.ExhaustedTurns -= 1
+        # If finished being exhausted, return to Normal State
+        if self.ExhaustedTurns is 0:
+            self.Tired = False
+            self.Exhausted = False
 
-                # 40% chance to place a tile
-                self.Tiles.append(Tile(x, y))
-                continue
+    def TryRun(self):
+        # State change from Ready to Tired after first run
+        if self.Tired is False:
+            self.Tired = True
+        else:
+            # Testing exhausion chance
+            exhaust = Utils.TryChance(self.ChanceToExhaust)
+            if exhaust:
+                self.Exhausted = True
+                self.ExhaustedTurns = 5
+            else:
+                # Increase the Chance to Exhaust up to 0.5
+                if self.ChanceToExhaust <= 0.5:
+                    # Keep the Chance to exhaust rounded to 2 decimal points
+                    self.ChanceToExhaust = round(self.ChanceToExhaust + 0.05, 2)
+                    if self.DebugMode:
+                        print("Player has chance: " + str(self.ChanceToExhaust) + " to Exhaust.")
 
-    def ShowAllObjects(self):
-        # Output all the objects
-        print("Tiles: " + str(len(self.Tiles)))
-        for obj in self.Tiles:
-            print(obj.GetString())
-
-        print("Walls: " + str(len(self.Walls)))
-        for obj in self.Walls:
-            print(obj.GetString())
-
-        print("Darts: " + str(len(self.TrapDarts)))
-        for obj in self.TrapDarts:
-            print(obj.GetString())
-
-        print("Doors: " + str(len(self.TrapDoors)))
-        for obj in self.TrapDoors:
-            print(obj.GetString())
-
-    # Check all objects in the grid to find
-    # the object at the given coordinates
-    def GetTileAt(self, pX, pY):
-        # Check the most likely culprits first
-        for tile in self.Tiles:
-            x, y = tile.GetCoords()
-            if x == pX and y == pY:
-                return tile
-        for wall in self.Walls:
-            x, y = wall.GetCoords()
-            if x == pX and y == pY:
-                return wall
-        for door in self.TrapDoors:
-            x, y = door.GetCoords()
-            if x == pX and y == pY:
-                return door
-        for dart in self.TrapDarts:
-            x, y = dart.GetCoords()
-            if x == pX and y == pY:
-                return dart
-
-    def ShowGrid(self):
-        finalString = ""
-        # Handle Columns
-        for y in range(0, self.Size, 1):
-            # Handle Rows
-            for x in range(0, self.Size, 1):
-                tile = self.GetTileAt(x,y)
-                finalString += tile.GetTile()
-            finalString += "\n\r"
-        print(finalString)
+    def Run(self):
+        # Test if we exhausted the player
+        self.TryRun()
--- TempleRunner.py
+++ TempleRunner.py
@@ -1,15 +1,49 @@
-
-
 # Import other Python files
-import Objects
+from Objects import *
+from GameEngine import *
 import Utils
 
+def main():
+    testingMode = 1
+    GameEng = GameEngine()
+    if testingMode == 0:
+        # Test generating a random level and picking a tile at random
+        print("Testing a random grid, and a random tile in that grid.")
+        GameEng.Grid.RandomizeLevel()
+        tile = GameEng.Grid.GetTileAt(6, 6)
+        # All Tiles
+        print("Got a tile at 6,6: " + str(tile))
+    elif testingMode == 1:
+        # Testing how far the Player Agent can run before he becomes exhausted
+        print("Testing how long it will take the Agent to become Exhausted.")
+        GameEng.Grid.RandomizeLevel()
+        GameEng.Grid.ShowGrid()
+        count = 0
+        while not GameEng.Grid.Player.Exhausted:
+            GameEng.Grid.Player.Run()
+            count += 1
+        print("Player exhasted after " + str(count) + " turns")
+    elif testingMode == 2:
+        print("Testing a players legal moves in a random grid.")
+        GameEng.Grid.RandomizeLevel()
+        GameEng.Grid.ShowGrid()
+        moves = GameEng.GetValidDirections()
+        print(moves)
 
+
+""" Run the Main function """
+main()
+
+
+
+"""
+    Code Graveyard - Remove after several commits if not used
+    
 def TestingCode():
     print("Hello World!")
 
     # Created an Object and print it out
-    tile = Objects.Tile()
+    tile = Tile()
     tile.Name = "Test Tile"
     print(" I just made a tile named: " + tile.Name)
 
@@ -28,19 +62,4 @@ def TestingCode():
     print("Coin Result: " + coin)
 
 
-def main():
-    testingMode = 1
-    grid = Objects.GameGrid()
-    grid.RandomizeLevel()
-    if testingMode == 0:
-        grid.ShowAllObjects()
-        tile = grid.GetTileAt(6,6)
-        print("Got a tile at 6,6: " + tile.GetString())
-    elif testingMode == 1:
-        grid.ShowGrid()
-
-
-
-
-""" Run the Main function """
-main()
+"""
